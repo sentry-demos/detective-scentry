0543372777d3f4b327f9640bb573bfc29890eb47
Chris Stavitsky <corridorr@gmail.com>
2022-10-03 16:23:02 -0700

    Specify what to do in case __SENTRY__ doesn't exist

--- dist/content.js
+++ dist/content.js
@@ -12,9 +12,18 @@ localStorage.rollbarLocation = localStorage.hasRollbar ? window.location.href : 
 localStorage.datadogLocation = localStorage.hasDatadog ? window.location.href : ''
 localStorage.logrocketLocation = localStorage.hasLogRocket ? window.location.href : ''
 
+// Metadata about Sentry Usage
 localStorage.usesSentryPerformance = usesSentryPerformance();
 
 function usesSentryPerformance() {
-	let options = __SENTRY__.hub.getClient().getOptions()
-	return !!options.tracesSampleRate || !!options.tracesSampler
+	if (__SENTRY__) {
+		let options = __SENTRY__.hub.getClient().getOptions()
+		return !!options.tracesSampleRate || !!options.tracesSampler
+	}
+
+	// The logic here is that performance did not exist in older SDKs (that used Raven instead
+	// of __SENTRY__). So, if __SENTRY__ is undefined, then we can assume the page doesn't use
+	// performance and return false.
+	// SDKs with performance available should have __SENTRY__ defined.
+	return false
 }
